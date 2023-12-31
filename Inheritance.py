538aec8fe7629f30fd9c2f2df71a95ebeb7fd03f
JOSHI HARSH <124500473+HarshJoshiKutch@users.noreply.github.com>
2023-12-31 08:32:30 +0530

    Add files via upload

--- Inheritance.py
+++ Inheritance.py
@@ -1,17 +1,50 @@
-class Mammal():
-    def walk(self):
-        print("walk")
-        
-class Dog:
-    def barks():
-        print("bark")
+# Inheritance means je parent class property ne inherit kariye chiye child class ma 
 
-class Cat:
-    def annoying():
-        print("Annoying")
+# class test:
+#     def test_meth(self):
+#         return "this is my first class"
 
-dog1 = Dog()
-dog1.walk()
+# class child_test(test):
+#     pass
 
-cat1= Cat()
-cat1.walk()     
+# child_test_obj = child_test()
+
+# print(child_test_obj.test_meth())
+
+# there are two types  1: multilevel and 2: multiple inheritance
+
+# multilevel inheritance
+
+# class class1:
+#     def test_class1(self):
+#         return "this is a method from class1"
+
+# class class2(class1):
+#     def test_class2(self):
+#         return "this is method form class 2"
+
+# class class3(class2):
+#     pass
+
+# obj_class3 = class3() # class3 na object ni help thi pan aapne te bhadhi aagad ni property access kari skaiye chiye
+
+# print(obj_class3.test_class2())
+
+
+# multiple inheiritence : multiple class mathi access kari sakiye single class ma 
+
+class class1:
+    def test_class1(self):
+        return "this is a class1"
+
+class class2:
+    def test_class2(self):
+        return "this is a class2"
+
+class class3(class1,class2):
+    pass
+
+obj_class3 = class3()
+
+print(obj_class3.test_class1())
+print(obj_class3.test_class2())
